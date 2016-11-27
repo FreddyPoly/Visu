2e0a76df8a6195308f841551c29f54b5104f4351
elou44 <elouarn.laine@gmail.com>
2016-11-28 00:34:25 +0100

    comparaison 2 pays

--- javascript/highchart.js
+++ javascript/highchart.js
@@ -1,10 +1,31 @@
-
-var lol = 1;
+var setHighchart;
 
 $(function () {
 
     function inverse(num){
         return(num*-1);
+    }
+
+    setHighchart = function(country1, country2){
+        window.chart1.series[0].setData((data_chart[country1].male).map(inverse));
+        window.chart1.series[0].name = "Homme ("+country1+")";
+        window.chart1.series[1].setData(data_chart[country1].female);
+        window.chart1.series[1].name = "Femme ("+country1+")";
+
+        if(country2 != null && country2 != '')
+        {
+            window.chart1.series[2].setData((data_chart[country2].male).map(inverse));
+            window.chart1.series[3].setData(data_chart[country2].female);
+            window.chart1.series[2].name = "Homme ("+country2+")";
+            window.chart1.series[3].name = "Femme ("+country2+")";
+            chart1.series[2].show();
+            chart1.series[3].show();
+        }
+        else
+        {
+            chart1.series[2].hide();
+            chart1.series[3].hide();
+        }
     }
 
     // Age categories
@@ -30,14 +51,14 @@ $(function () {
                 opposite: true,
                 reversed: false,
                 categories: categories,
-                linkedTo: 0,
+                linkedTo:  0,
                 labels: {
                     step: 1
                 }
             }],
             yAxis: {
                 title: {
-                    text: null
+                    text: 'Homme / Femme'
                 },
                 labels: {
                     formatter: function () {
@@ -48,29 +69,45 @@ $(function () {
 
             plotOptions: {
                 series: {
-                    stacking: 'normal'
+                    stacking: 'normal',
+                    pointWidth: 15
                 }
+            },
+
+            legend: {
+                enabled: false
             },
 
             tooltip: {
                 formatter: function () {
-                    return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' +
-                        'Population: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1);
+                    return '<b>' + this.series.name + ', ' + this.point.category + '</b><br/>' +
+                        'Temps: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1)+ 'h';
                 }
             },
 
             series: [{
                 name: 'Homme',
-                data: [-2.2, -2.2, -2.3, -2.5]
+                data: [-2,-2,-2,-2],
+                stack : 'country1'
             }, {
                 name: 'Femme',
-                data: [2.1, 2.0, 2.2, 2.4]
+                data: [2,2,2,2],
+                stack : 'country1'
+            },
+            {
+                name: 'Homme',
+                data: [-2,-2,-2,-2],
+                stack : 'country2'
+            },
+            {
+                name: 'Femme',
+                data: [2,2,2,2],
+                stack : 'country2'
             }]
         });
-
-    //chart1.series[0].setData([-2.2, -2.2, -2.3, -5]);
-    chart1.series[0].setData((data_chart['france'].male).map(inverse));
-    console.log(data_chart);
+        
+        chart1.series[2].hide();
+        chart1.series[3].hide();
 
     });
 
