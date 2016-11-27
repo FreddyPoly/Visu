e158305c64ca7c44e065fafc51d833e5a2032209
Jonathan Pelletier <Jonathan Pelletier>
2016-11-27 12:52:46 +0100

    Click nom pays

--- javascript/main.js
+++ javascript/main.js
@@ -4,18 +4,14 @@
 	$(document).ready(function() {
 		// CSSMap;
 		$("#map-europe").CSSMap({
-		  "size": 430
-		});
+			"size": 750,
+			onClick: function(e) {
+				var rLink = e.children("A").eq(0).attr("href"),
+				    rText = e.children("A").eq(0).text(),
+				    rClass = e.attr("class").split(" ")[0];
 
-		$(".eu13").find("span").each(function() {
-			console.log($(this));
-			$(this).find("span").each(function() {
-				console.log("2eme span "+$(this));
-				console.log($(this));
-				$(this).click(function() {
-					console.log("France");
-				});
-			});
+				console.log(rText);
+			}
 		});
 	});
 
