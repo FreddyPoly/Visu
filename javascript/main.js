b5b067ab7c5fb7e7c53c7407e026c856d6277d14
Jonathan Pelletier <Jonathan Pelletier>
2016-11-27 21:49:41 +0100

    Map responsive ?

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,37 @@
 (function() {
 	'use strict';
 
+	// Réajuste la taille de la map au viewport actuel
+	function resize_map() {
+		// Réajustement de la taille de la map à chaque changement de résolution
+		var height = jQuery(window).height();
+		var width = jQuery(window).width();
+
+		var new_height = height * 100 / 648;
+		var new_width = width * 135 / 1366;
+
+		// Valeurs minimales
+		if(new_height < 100)
+			new_height = 125;
+
+		if(new_width < 100)
+			new_width = 125;
+
+		$("#bigger_map").height(new_height+"%");
+		$("#bigger_map").width(new_width+"%");
+
+		//console.log("Height avec "+height+"px qui donne "+new_height);
+		//console.log("Width avec "+width+"px qui donne "+new_width);
+	}
+
+	// A chaque changement de la taille du viewport on réajuste la taille de la map
+	$(window).resize(function() {
+		resize_map();
+	});
+
 	$(document).ready(function() {
+		resize_map();
+
 		// CSSMap;
 		$("#map-europe").CSSMap({
 			"size": 1450,
