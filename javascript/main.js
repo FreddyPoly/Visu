646174a47c974952aa4f8cf225ecfc2e11fc4bbe
Jonathan Pelletier <Jonathan Pelletier>
2016-11-28 22:34:04 +0100

    Plus de prob de map qui remonte

--- javascript/main.js
+++ javascript/main.js
@@ -37,7 +37,6 @@
 			"size": 1450,
 			"responsive": "auto",
 			onClick: function(e) {
-				resize_map();
 				var rLink = e.children("A").eq(0).attr("href"),
 				    rText = e.children("A").eq(0).text(),
 				    rClass = e.attr("class").split(" ")[0];
