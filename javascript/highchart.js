20b99c24609ca5ca113c13734f71c612ee913757
elou44 <elouarn.laine@gmail.com>
2016-12-02 01:56:39 +0100

    deux pies connectés

--- javascript/highchart.js
+++ javascript/highchart.js
@@ -20,9 +20,37 @@ $(function () {
             window.chart1.series[3].name = "Femme ("+country2+")";
             chart1.series[2].show();
             chart1.series[3].show();
+
+            var sum_male = data_chart[country1].male.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var sum_female = data_chart[country1].female.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var percent_male = sum_male / (sum_male + sum_female);
+            var percent_female = sum_female / (sum_male + sum_female);
+            window.pie1_data.series[0].setData([sum_male,sum_female]);
+
+            window.pie1_data.setTitle({text:country1});
+
+            document.getElementById("th_pie2").className = "pie_data";
+
+            sum_male = data_chart[country2].male.reduce(function(pv, cv) { return pv + cv; }, 0);
+            sum_female = data_chart[country2].female.reduce(function(pv, cv) { return pv + cv; }, 0);
+            percent_male = sum_male / (sum_male + sum_female);
+            percent_female = sum_female / (sum_male + sum_female);
+            window.pie2_data.series[0].setData([sum_male,sum_female]);
+
+            window.pie2_data.setTitle({text:country2});
         }
         else
         {
+            var sum_male = data_chart[country1].male.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var sum_female = data_chart[country1].female.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var percent_male = sum_male / (sum_male + sum_female);
+            var percent_female = sum_female / (sum_male + sum_female);
+            window.pie1_data.series[0].setData([sum_male,sum_female]);
+
+            window.pie1_data.setTitle({text:country1});
+
+            document.getElementById("th_pie2").className = "display_none";
+
             chart1.series[2].hide();
             chart1.series[3].hide();
         }
@@ -106,6 +134,109 @@ $(function () {
         
         chart1.series[2].hide();
         chart1.series[3].hide();
+
+        window.pie1_data = Highcharts.chart("pie1", {
+            chart: {
+                margin: [0, 0, 0, 0],
+                spacingTop: 0,
+                spacingBottom: 0,
+                spacingLeft: 0,
+                spacingRight: 0,
+                backgroundColor:'rgba(255, 255, 255, 0.0)',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:1.0f}%</b><b> ({point.y:.1f}h)</b>'
+            },
+            exporting: { enabled: false },
+            title: {
+                text: 'null'
+            },
+            credits: {
+                enabled: false
+            },
+            plotOptions: {
+                pie: {
+                    size:'80%',
+                    showInLegend: false,
+                    dataLabels: {
+
+                        enabled: true,
+                        distance: -35,
+                        style: {
+                            color: 'white'
+                        }
+                    },
+                },
+            },
+            series: [{
+                name: 'Répartition H/F',
+                colorByPoint: true,
+                data: [{
+                    name: 'Homme',
+                    y: 50
+                }, {
+                    name: 'Femme',
+                    y: 50,
+                }]
+            }]
+        });
+
+        window.pie2_data = Highcharts.chart("pie2", {
+            chart: {
+                margin: [0, 0, 0, 0],
+                spacingTop: 0,
+                spacingBottom: 0,
+                spacingLeft: 0,
+                spacingRight: 0,
+                backgroundColor:'rgba(255, 255, 255, 0.0)',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:1.0f}%</b><b> ({point.y:.1f}h)</b>'
+            },
+            exporting: { enabled: false },
+            title: {
+                text: 'null'
+            },
+            credits: {
+                enabled: false
+            },
+            plotOptions: {
+                pie: {
+                    size:'80%',
+                    showInLegend: false,
+                    dataLabels: {
+
+                        enabled: true,
+                        distance: -35,
+                        style: {
+                            color: 'white'
+                        }
+                    },
+                },
+            },
+            series: [{
+                name: 'Répartition H/F',
+                colorByPoint: true,
+                data: [{
+                    name: 'Homme',
+                    y: 50,
+                    color: Highcharts.getOptions().colors[2]
+                }, {
+                    name: 'Femme',
+                    y: 50,
+                    color: Highcharts.getOptions().colors[3]
+                }]
+            }]
+        });
+
 
     });
 
